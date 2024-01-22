c570e675b174d365d8f3733ee4c0fe18bdc93e61
Aleksander Afanasyev <afonia.marcii@gmail.com>
2024-01-22 13:49:49 +0300

    fix: del pinia

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,8 @@ import { createApp } from 'vue';
 import './scss/main.scss';
 import App from './App.vue';
 import router from './router';
-import { createPinia } from 'pinia';
 
 const app = createApp(App);
 app.use(router);
-app.use(createPinia());
 
 app.mount('#app');
